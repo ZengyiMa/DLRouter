673e84d92f95cd3c86e97737eaed7a0e9477a207
famulei <mazengyi@famulei.com>
2016-09-02 14:19:32 +0800

    Initial Commit

---
+++ DLRouter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DLRouter
+//
+//  Created by famulei on 9/2/16.
+//  Copyright © 2016 famulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DLRouter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DLRouter
+//
+//  Created by famulei on 9/2/16.
+//  Copyright © 2016 famulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
